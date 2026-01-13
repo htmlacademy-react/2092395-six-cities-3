556be7b2acc85f41ddccd5041533bfab0ef5b046
vasilii <v.verkin@gmail.com>
2026-01-13 17:26:48 +0300

    Начинает настройку авторизации

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -39,15 +39,21 @@ export default function Layout() {
                       <a className="header__nav-link header__nav-link--profile" href="#">
                         <div className="header__avatar-wrapper user__avatar-wrapper">
                         </div>
-                        <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                        <span className="header__favorite-count">3</span>
+                        {authorizationStatus === AuthorizationStatus.Auth ? (
+                          <>
+                            <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                            <span className="header__favorite-count">3</span>
+                          </>
+                        ) : <span className="header__login">Sign in</span>}
                       </a>
                     </li>
-                    <li className="header__nav-item">
-                      <a className="header__nav-link" href="#">
-                        <span className="header__signout">Sign out</span>
-                      </a>
-                    </li>
+                    {authorizationStatus === AuthorizationStatus.Auth ? (
+                      <li className="header__nav-item">
+                        <a className="header__nav-link" href="#">
+                          <span className="header__signout">Sign out</span>
+                        </a>
+                      </li>
+                    ) : null}
                   </ul>
                 </nav>
               ) : null
